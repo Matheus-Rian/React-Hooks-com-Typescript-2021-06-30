8657db5114dd77cc45b7cbe589bfa414a20f1fc9
Matheus Rian <souzamatheusrian@gmail.com>
2022-07-29 18:38:21 -0300

    feat: add useLocalStorage in component Toggle

--- src/components/Toggle.tsx
+++ src/components/Toggle.tsx
@@ -1,11 +1,13 @@
+import { useLocalStorage } from "../hooks/useLocalStorage";
 import { useToggle } from "../hooks/useToggle";
 
 const Toggle = (props: any) => {
   const [toggled, setToggle] = useToggle(false);
+  const [savedToggle, setSavedToggle] = useLocalStorage('darkmode', false);
 
   return (
     <div className="dark-mode-toggle">
-      <button type="button" onClick={setToggle}>
+      <button type="button" onClick={() => setSavedToggle(false)}>
         ☀
       </button>
       <span className="toggle-control">
@@ -18,7 +20,7 @@ const Toggle = (props: any) => {
         />
         <label htmlFor="dmcheck" />
       </span>
-      <button type="button" onClick={setToggle}>
+      <button type="button" onClick={() => setSavedToggle(true)}>
         ☾
       </button>
     </div>
