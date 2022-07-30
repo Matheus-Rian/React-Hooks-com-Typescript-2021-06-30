35e2f47d3798e4b59e431df844fc33aadcc50a4e
Matheus Rian <souzamatheusrian@gmail.com>
2022-07-29 23:33:39 -0300

    feat: add call api in Playlist

--- src/pages/Playlist.tsx
+++ src/pages/Playlist.tsx
@@ -1,40 +1,77 @@
-import React, { useState } from "react";
-import { responseDemo } from "../mock";
+import { useState } from "react";
+import { AudioPlayerProvider } from "react-use-audio-player";
+import Player from "../components/Player";
+import { RequestStatus, useFetch } from "../hooks/useFetch";
+import { axiosOptions, tracksUrl } from "../service";
+
+interface Track {
+  uri: string;
+  index: number;
+  paused: boolean;
+  id: number;
+  percentage: number;
+  played: boolean;
+  currentTime: number;
+  playing: boolean;
+  artist: string;
+  title: string;
+  duration: number;
+  permalink_url: string;
+  favoritings_count: number;
+  stream_url: string;
+  artwork_url: string;
+}
 
 function Playlist() {
-  const [data] = useState(responseDemo);
+  const { status, data, error } = useFetch<Track[]>(tracksUrl, axiosOptions);
+  const [track, setTrack] = useState<Track>();
+
+  if (status === RequestStatus.fetching) {
+    return <h1>Loading...</h1>
+  }
+
+  if (error) {
+    return <h1>error</h1>
+  }
 
   return (
-    <div className="playlists">
-      <div className="list">
-        <ul className="track-list">
-          {data?.map((track, index) => (
-            <li key={`track-${index}`} className="row">
-              <button className="btn" onClick={() => console.log(track)}>
-                <div className="album">
-                  <img
-                    className="album__cover"
-                    width={80}
-                    height={80}
-                    src={track.artwork_url}
-                    alt={`Album artwork from track ${track.title}.`}
-                  />
-                </div>
-                <div className="info">
-                  <h2 className="info__track">{track.title}</h2>
-                  <span className="info__artist">{track.artist}</span>
-                </div>
+      <div className="playlists">
+        <div className="list">
+          <ul className="track-list">
+            {data?.map((track, index) => (
+              <li key={`track-${index}`} className="row">
+                <button className="btn" onClick={() => console.log(track)}>
+                  <div className="album">
+                    <img
+                      className="album__cover"
+                      width={80}
+                      height={80}
+                      src={track.artwork_url}
+                      alt={`Album artwork from track ${track.title}.`}
+                    />
+                  </div>
+                  <div className="info">
+                    <h2 className="info__track">{track.title}</h2>
+                    <span className="info__artist">{track.artist}</span>
+                  </div>
 
-                <div className="more">
-                  <i className="fas fa-ellipsis-v" />
-                </div>
-              </button>
-            </li>
-          ))}
-        </ul>
+                  <div className="more">
+                    <i className="fas fa-ellipsis-v" />
+                  </div>
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <AudioPlayerProvider>
+          <Player 
+            title={track?.title}
+            file={track?.stream_url}
+            artWork={track?.artwork_url}
+          />
+        </AudioPlayerProvider>
       </div>
-    </div>
-  );
+    );
 }
 
 export default Playlist;
